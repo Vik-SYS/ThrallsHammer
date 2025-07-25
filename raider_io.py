9fc864332db4bdc19759da959e8f2f74fd22b1e4
Vik-SYS <kaguya.chekhova@gmail.com>
2025-07-25 02:42:43 -0500

    Added M+ Profile API puller

--- raider_io.py
+++ raider_io.py
@@ -8,3 +8,20 @@ async def get_weekly_affixes(region-"us", locale-"en"):
                  return await resp.json()
              else:
                  raise Exception(f"Failed to get affix(s): {resp.status}")
+
+async def get_mythic_plus_profile(name, realm, region="us"):
+    base_url = "https://raider.io/api/v1/characters/profile"
+    params = {
+        "region": region,
+        "realm": realm,
+        "name": name,
+        "fields": "mythic_plus_scores,mythic_plus_best_runs"
+
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(base_url, params=params) as resp:
+            if resp.status == 200:
+                return await resp.json()
+            else:
+                raise Exception(f"Failed to get Raider.IO data: {resp.status}")
